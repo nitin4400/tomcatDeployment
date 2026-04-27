a05d36ce4dc5e6421e19fad6bce161553136c67e
Nitin <nitinkumarmahli3@gmail.com>
2026-04-27 17:23:45 +0530

    used get mapping in the place of requestmapping

--- src/main/java/com/example/ForDeployment/TController.java
+++ src/main/java/com/example/ForDeployment/TController.java
@@ -1,12 +1,12 @@
 package com.example.ForDeployment;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TController {
 
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String hello()
 	{
 		return "hello";
