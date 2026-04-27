53ae7caf36c57f431d367090a01152730c798e20
Nitin <nitinkumarmahli3@gmail.com>
2026-04-27 15:39:00 +0530

    changed code in main file of project to check if it work or not

--- src/main/java/com/example/ForDeployment/ForDeploymentApplication.java
+++ src/main/java/com/example/ForDeployment/ForDeploymentApplication.java
@@ -2,18 +2,12 @@ package com.example.ForDeployment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ForDeploymentApplication extends SpringBootServletInitializer { // 1. Extend this class
+public class ForDeploymentApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) { // 2. Add this method
-        return builder.sources(ForDeploymentApplication.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ForDeploymentApplication.class, args);
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(ForDeploymentApplication.class, args);
-    }
 }
