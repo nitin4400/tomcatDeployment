4c7514dca8db9e160f33f38f148b210623e2d463
Nitin <nitinkumarmahli3@gmail.com>
2026-04-27 09:59:03 +0530

    pushing war file

---
+++ src/main/java/com/example/ForDeployment/ForDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ForDeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ForDeployment/ForDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ForDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
