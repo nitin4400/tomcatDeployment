a460ba7aec44c90a54a0053c4eb5f3cf58851600
Nitin <nitinkumarmahli3@gmail.com>
2026-05-04 10:14:07 +0530

    made some changes in controller

--- src/main/java/com/example/ForDeployment/TController.java
+++ src/main/java/com/example/ForDeployment/TController.java
@@ -1,14 +1,19 @@
 package com.example.ForDeployment;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TController {
 
 	@GetMapping("/hello")
 	public String greeting()
 	{
-		return "index.html";
+		return "index";
+	}
+	@GetMapping("/welcome")
+	public String welcome()
+	{
+		return "hii";
 	}
 }
